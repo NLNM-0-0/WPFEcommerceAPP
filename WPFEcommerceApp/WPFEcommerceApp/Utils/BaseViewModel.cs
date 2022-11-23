03d2013b12a1be1da6e2f6c4cb22d4d9b8075024
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2022-11-23 17:51:36 +0700

    Add: Some base abstract class  for ViewModel

---
+++ WPFEcommerceApp/WPFEcommerceApp/Utils/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using PropertyChanged;
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace WPFEcommerceApp
+{
+    /// <summary>
+    /// A base view model that fires Property Changed events as needed
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    { 
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
