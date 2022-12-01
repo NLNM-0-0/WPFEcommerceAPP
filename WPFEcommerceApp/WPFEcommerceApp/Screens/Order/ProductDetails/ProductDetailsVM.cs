bb57c56f8d2acf0dc644bac1ee6ffb0d4b96991d
vuhoang.vh <21520243@gm.uit.edu.vn>
2022-12-02 03:25:31 +0700

    dev: Product Detail screen

---
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/Order/ProductDetails/ProductDetailsVM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFEcommerceApp {
+    public class ProductDetailsVM :BaseViewModel {
+		private int status;
+
+		public int Status {
+			get { return status; }
+			set { status = value; OnPropertyChanged(); }
+		}
+
+		private ObservableCollection<bool> orderStatus;
+
+		public ObservableCollection<bool> OrderStatus {
+			get { return orderStatus; }
+			set { orderStatus = value; OnPropertyChanged(); }
+		}
+		public ObservableCollection<BProduct> ShopProduct { get; set; }
+		private Order orderDetail;
+
+		public Order OrderDetail {
+			get { return orderDetail; }
+			set { orderDetail = value; OnPropertyChanged(); }
+		}
+
+
+		public ProductDetailsVM() {
+			OrderDetail = new Order();
+			if(OrderDetail.Status == "Processing") {
+				Status = 1;
+			}
+			else if(OrderDetail.Status == "Dellivering") {
+				Status=2;
+			}
+			else if(OrderDetail.Status == "Dellivered") {
+				Status=3;
+			}
+			else if(OrderDetail.Status == "Cancelled") {
+				Status=0;
+			}
+			else Status=4;
+			ShopProduct = new ObservableCollection<BProduct>(OrderDetail.ShopProduct);
+			OrderStatus = new ObservableCollection<bool>() {
+				false, false, false, false
+			};
+			for(int i = 0; i < Status; i++) {
+				OrderStatus[i] = true;
+			}
+        }
+
+    }
+}
