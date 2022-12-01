82e6cc87f8081b0369e239146c7c95eecbe1a890
vuhoang.vh <21520243@gm.uit.edu.vn>
2022-12-02 03:27:16 +0700

    fix: Test UserView

--- WPFEcommerceApp/WPFEcommerceApp/Screens/UserView/UserViewModel.cs
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/UserView/UserViewModel.cs
@@ -5,6 +5,13 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
+using WPFEcommerceApp.Screens.Payment.Checkout;
+using WPFEcommerceApp.Screens.Payment.SuccessScreen;
 
 namespace WPFEcommerceApp
 {
@@ -22,10 +29,9 @@ namespace WPFEcommerceApp
 
         }
 
-
         public ObservableCollection<MainItem> MainItems { get; private set; }
-
-
+        public ICommand OnChangeScreen { get; set; }
+        public ICommand OnSuccessPayment { get; set; }
         public UserViewModel()
         {
             MainItems = new ObservableCollection<MainItem>();
@@ -44,7 +50,33 @@ namespace WPFEcommerceApp
                 "ShopMain",
                 typeof(ShopMain)
             ));
-            SelectedItem = MainItems[0];
+            MainItems.Add(new MainItem(
+                "CheckoutScreen",
+                typeof(CheckoutScreen)));
+            MainItems.Add(new MainItem(
+                "OrderScreen",
+                typeof(OrderScreen)));
+            MainItems.Add(new MainItem(
+                "SuccessScreen",
+                typeof(SuccessScreen)));
+            MainItems.Add(new MainItem(
+                "ProductDetails",
+                typeof(ProductDetails)));
+            SelectedItem = MainItems[6];
+
+            OnChangeScreen = new RelayCommand<object>((p) => true, (p) => {
+                string nav = ((p as ListView).SelectedItem as ButtonItem).NavLink;
+                for(int i = 0; i < MainItems.Count; i++) {
+                    if(MainItems[i].Name == nav) {
+                        SelectedItem = MainItems[i];
+                        break;
+                    }
+                }
+            });
+            OnSuccessPayment = new RelayCommand<object>((p) => true, (p) => {
+                DialogHost.CloseDialogCommand.Execute(p, (p as IInputElement));
+                SelectedItem = MainItems[5];
+            });
         }
     }
 }
