ab3d7a85f2a53be54a60d6446062b566c5f20cf3
vuhoang.vh <21520243@gm.uit.edu.vn>
2022-12-03 06:01:00 +0700

    dev: Add navigation props | update: BaseViewModel

---
+++ WPFEcommerceApp/WPFEcommerceApp/Services/ParamNavigationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFEcommerceApp {
+    public class ParamNavigationService<TParameter, TViewModel>
+        where TViewModel : BaseViewModel{
+        private readonly NavigationStore _navigationStore;
+        private readonly CreateViewModel<TParameter, TViewModel> _createViewModel;
+
+        public ParamNavigationService(NavigationStore navigationStore, CreateViewModel<TParameter, TViewModel> createViewModel) {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public void Navigate(TParameter parameter) {
+            _navigationStore.CurrentViewModel = _createViewModel(parameter);
+        }
+    }
+}
---
+++ WPFEcommerceApp/WPFEcommerceApp/Utils/CreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFEcommerceApp {
+    public delegate TViewModel CreateViewModel<TViewModel>() where TViewModel : BaseViewModel;
+
+    public delegate TViewModel CreateViewModel<TParameter, TViewModel>(TParameter parameter) where TViewModel : BaseViewModel;
+}
