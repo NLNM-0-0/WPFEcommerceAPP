df5ed982c91534f47e6e8087707b1e1729b1423d
kimthu09 <21521495@gm.uit.edu.vn>
2022-12-03 22:53:59 +0700

    Add: Connection to SQL

---
+++ WPFEcommerceApp/DataAccessLayer/GenericDataRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class GenericDataRepository<T> : IGenericDataRepository<T> where T : class
+    {
+        // Add
+        public virtual void Add(params T[] items)
+        {
+            using (var context = new EcommerceAppEntities())
+            {
+                foreach (T item in items)
+                {
+                    context.Entry(item).State = EntityState.Added;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public virtual IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+        {
+            List<T> list;
+            using (var context = new EcommerceAppEntities())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                //Apply eager loading
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                list = dbQuery
+                    .AsNoTracking()
+                    .ToList<T>();
+            }
+            return list;
+        }
+
+        public virtual IList<T> GetList(Func<T, bool> where,
+             params Expression<Func<T, object>>[] navigationProperties)
+        {
+            List<T> list;
+            using (var context = new EcommerceAppEntities())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                //Apply eager loading
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                list = dbQuery
+                    .AsNoTracking()
+                    .Where(where)
+                    .ToList<T>();
+            }
+            return list;
+        }
+
+        public virtual T GetSingle(Func<T, bool> where,
+             params Expression<Func<T, object>>[] navigationProperties)
+        {
+            T item = null;
+            using (var context = new EcommerceAppEntities())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                //Apply eager loading
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                item = dbQuery
+                    .AsNoTracking() //Don't track any changes for the selected item
+                    .FirstOrDefault(where); //Apply where clause
+            }
+            return item;
+        }
+
+        public void Remove(params T[] items)
+        {
+
+            using (var context = new EcommerceAppEntities())
+            {
+                foreach (T item in items)
+                {
+                    context.Entry(item).State = EntityState.Deleted;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(params T[] items)
+        {
+            using (var context = new EcommerceAppEntities())
+            {
+                foreach (T item in items)
+                {
+                    context.Entry(item).State = EntityState.Modified;
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
