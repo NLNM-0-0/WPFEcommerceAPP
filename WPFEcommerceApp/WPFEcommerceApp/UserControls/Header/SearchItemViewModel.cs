acd5a3325f4854ac1036a2d066aa12990c813f56
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2022-12-07 19:30:16 +0700

    Fix: Remove UserControl to UserControls

---
+++ WPFEcommerceApp/WPFEcommerceApp/UserControls/Header/SearchItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WPFEcommerceApp
+{
+    public class SearchItemViewModel:BaseViewModel
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        private ImageSource _sourceImage;
+        public ImageSource SourceImage
+        {
+            get { return _sourceImage; }
+            set { _sourceImage = value; OnPropertyChanged(); }
+        }
+
+        public SearchItemViewModel()
+        {
+            SourceImage = null;
+            Name = "A product or shop name";
+        }
+    }
+}
---
+++ WPFEcommerceApp/WPFEcommerceApp/UserControls/ProductBlock/ImageProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+namespace WPFEcommerceApp
+{
+    /// <summary>
+    /// This class will be deleted after having database
+    /// </summary>
+    partial class ImageProduct:BaseViewModel
+    {
+        public string Path { get; set; }
+        public Image Image { get; private set; }
+        public ImageProduct(string path = null)
+        {
+            if (path != null)
+            {
+                Path = path;
+            }
+            else
+            {
+                Path = "http://www.sitech.co.id/assets/img/products/default.jpg";
+            }
+            Image = new Image();
+            Image.Source = new BitmapImage(new Uri(Path));
+        }
+    }
+}
