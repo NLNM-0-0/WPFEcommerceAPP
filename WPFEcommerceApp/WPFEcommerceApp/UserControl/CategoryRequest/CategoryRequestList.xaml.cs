2d02b6593c917fbd5fe4167b33166c18cb0665a9
kimthu09 <21521495@gm.uit.edu.vn>
2022-12-09 14:46:42 +0700

    add: category request usercontrol

---
+++ WPFEcommerceApp/WPFEcommerceApp/UserControl/CategoryRequest/CategoryRequestList.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows.Controls;
+
+
+namespace WPFEcommerceApp
+{
+    /// <summary>
+    /// Interaction logic for ShopRequestList.xaml
+    /// </summary>
+    public partial class CategoryRequestList : UserControl
+    {
+        public CategoryRequestList()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPFEcommerceApp/WPFEcommerceApp/UserControl/CategoryRequest/CategoryRequestListViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.ObjectModel;
+
+namespace WPFEcommerceApp
+{
+    public class CategoryRequestListViewModel: BaseViewModel
+    {
+        private ObservableCollection<CategoryRequestItemViewModel> _items;
+        public ObservableCollection<CategoryRequestItemViewModel> Items
+        {
+            get { return _items; }
+            set { _items = value; OnPropertyChanged(); }
+        }
+
+        public CategoryRequestListViewModel()
+        {
+
+        }
+    }
+}
