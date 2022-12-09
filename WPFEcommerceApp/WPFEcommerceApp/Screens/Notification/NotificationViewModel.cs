48496c8bf10fd05483b7a0d481aefc9372a5d391
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2022-12-09 19:13:17 +0700

    Add: Notification Screen

---
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/Notification/NotificationViewModel.cs
@@ -0,0 +1,51 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using WPFEcommerceApp.Models;
+
+namespace WPFEcommerceApp
+{
+    internal class NotificationViewModel : BaseViewModel
+    {
+        public GenericDataRepository<Models.Notification> NoteRepo;
+        public int ID = 1;
+
+
+
+        private ObservableCollection<NotificationBlock> notifications;
+        public ObservableCollection<NotificationBlock> Notifications
+        {
+            get { return notifications; }
+            set
+            {
+                notifications = value;
+                OnPropertyChanged();
+            }
+        }
+        public NotificationViewModel()
+        {
+            NoteRepo = new GenericDataRepository<Models.Notification>();
+            Load();
+        }
+        private async void Load()
+        {
+
+            var noteList =new ObservableCollection<Models.Notification>( await NoteRepo.GetListAsync(item => item.IdReceiver == ID,
+                                                        item=>item.MUser
+                                                        ));
+
+            Notifications = new ObservableCollection<NotificationBlock>(noteList.Select(item => new NotificationBlock
+            {
+                AvaImage = new BitmapImage(new Uri(item.MUser.SourceImageAva)),
+                UserName = item.MUser.Name,
+                Date = item.Date.ToString(),
+                NotificationContent = item.Content
+            })) ;
+        }
+    }
+}
