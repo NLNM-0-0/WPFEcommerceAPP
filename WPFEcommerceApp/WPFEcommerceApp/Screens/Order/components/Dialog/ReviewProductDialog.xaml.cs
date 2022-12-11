ea5111e45666e1df1fca2caf2045243459896a0a
vuhoang.vh <21520243@gm.uit.edu.vn>
2022-12-11 22:02:02 +0700

    dev: ReviewProductDialog

--- WPFEcommerceApp/WPFEcommerceApp/Screens/Order/components/Dialog/ReviewProductDialog.xaml.cs
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/Order/components/Dialog/ReviewProductDialog.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,12 +22,47 @@ namespace WPFEcommerceApp {
     /// <summary>
     /// Interaction logic for ReviewProductDialog.xaml
     /// </summary>
-    public partial class ReviewProductDialog : UserControl {
+    public partial class ReviewProductDialog : UserControl, INotifyPropertyChanged {
+
+
+        private int _value;
+
+        public int Value {
+            get { return _value; }
+            set { _value = value; OnPropertyChanged(); }
+        }
+
+        private ICommand onOK;
+
+        public ICommand OnOK {
+            get { return onOK; }
+            set { onOK = value; OnPropertyChanged(); }
+        }
+
+
+
+        public Product Param {
+            get { return (Product)GetValue(ParamProperty); }
+            set { SetValue(ParamProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Param.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ParamProperty =
+            DependencyProperty.Register("Param", typeof(Product), typeof(ReviewProductDialog), new PropertyMetadata(null));
+
+
+
         public ReviewProductDialog() {
             InitializeComponent();
+            DataContext = this;
+            OnOK = new RelayCommand<object>(p => true, p => {
+                //do something with Product
+            });
         }
-        private async void Button_Click(object sender, RoutedEventArgs e) {
 
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
