ff81e9b9000082ff067ee7931adfb9197b19163b
kimthu09 <21521495@gm.uit.edu.vn>
2022-12-14 22:36:21 +0700

    dev: add ads dialog

---
+++ WPFEcommerceApp/WPFEcommerceApp/UserControls/Dialogs/AddAdsDialog.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows.Controls;
+
+
+namespace WPFEcommerceApp
+{
+    /// <summary>
+    /// Interaction logic for AddBranDialog.xaml
+    /// </summary>
+    public partial class AddAdsDialog : UserControl
+    {
+        public AddAdsDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
