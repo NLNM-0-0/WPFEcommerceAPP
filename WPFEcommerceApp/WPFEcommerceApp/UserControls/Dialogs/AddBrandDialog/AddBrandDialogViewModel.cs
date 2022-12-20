db9ce66a122742d9e2df746671277bb79c1dbdba
NLNM-0-0 <113424382+NLNM-0-0@users.noreply.github.com>
2022-12-20 18:16:57 +0700

    Update: update database to AddBrandDialog

--- WPFEcommerceApp/WPFEcommerceApp/UserControls/Dialogs/AddBrandDialog/AddBrandDialogViewModel.cs
+++ WPFEcommerceApp/WPFEcommerceApp/UserControls/Dialogs/AddBrandDialog/AddBrandDialogViewModel.cs
@@ -1,15 +1,22 @@
-﻿using MaterialDesignThemes.Wpf;
+﻿using DataAccessLayer;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
+using WPFEcommerceApp.Models;
 
 namespace WPFEcommerceApp
 {
     public class AddBrandDialogViewModel : BaseViewModel
     {
+        private readonly AccountStore _accountStore;
+        private GenericDataRepository<Models.BrandRequest> brandRequestReposition = new GenericDataRepository<BrandRequest>();
         private string brandName = "";
         public string BrandName
         {
@@ -30,17 +37,49 @@ namespace WPFEcommerceApp
                 OnPropertyChanged();
             }
         }
+        private string stringCloseDialog ="";
         public ICommand RequestBrandCommand { get; set; }
-        public AddBrandDialogViewModel()
+        public ICommand KeyDownEnterCommand { get; set; }
+        public AddBrandDialogViewModel(AccountStore accountStore)
         {
-            RequestBrandCommand = new RelayCommand<object>((p) => { return p != null; }, (p) =>
-            {
-                //Lưu dữ liêu xuống data base
-
-                //Tắt dialogHost
-                var closeDialog = DialogHost.CloseDialogCommand;
-                closeDialog.Execute(null, null);
+            _accountStore = accountStore;
+            RequestBrandCommand = new RelayCommandWithNoParameter(()=>
+            { 
+                Task task = Task.Run(async () => await AddBrandRequest());
+                while (!task.IsCompleted) ;
+                NotificationDialog notification = new NotificationDialog()
+                {
+                    Header = "Notification",
+                    ContentDialog = stringCloseDialog
+                };
+                DialogHost.Show(notification, "Notification");
             });
+            KeyDownEnterCommand = new RelayCommand<object>((p) => p != null, (p) =>
+            {
+                System.Windows.Controls.Button button = p as System.Windows.Controls.Button;
+                if(button.IsEnabled)
+                {
+                    button.Command.Execute(null);    
+                }
+            });
+        }
+        private async Task AddBrandRequest()
+        {
+            try
+            {
+                await brandRequestReposition.Add(new BrandRequest()
+                {
+                    Id =  await GenerateID.Gen(typeof(Brand)),
+                    IdShop = _accountStore.CurrentAccount.Id,
+                    Name = BrandName,
+                    Reason = this.Reason
+                });
+                stringCloseDialog = "Update sucessfully. Please wait for us to apply.";
+            }
+            catch
+            {
+                stringCloseDialog = "There is exist this brand in the brand request. Please wait for us to apply.";
+            }
         }
     }
 }
