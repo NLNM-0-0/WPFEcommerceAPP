d0663c8ef8e207904b483ce29876f28c4fbfe268
NLNM-0-0 <113424382+NLNM-0-0@users.noreply.github.com>
2022-12-20 18:43:28 +0700

    add: add database to ProductInfoPortraitBanned fix: design the UI again based on database

--- WPFEcommerceApp/WPFEcommerceApp/UserControls/Dialogs/ProductInfoPortraitBanned/ProductInfoPortraitBannedViewModel.cs
+++ WPFEcommerceApp/WPFEcommerceApp/UserControls/Dialogs/ProductInfoPortraitBanned/ProductInfoPortraitBannedViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MaterialDesignThemes.Wpf;
+﻿using DataAccessLayer;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,9 @@ namespace WPFEcommerceApp
 {
     public class ProductInfoPortraitBannedViewModel : BaseViewModel
     {
+        private readonly AccountStore _accountStore;
+        private GenericDataRepository<Models.Product> productRepository = new GenericDataRepository<Models.Product>();
+        public ICommand ContactUsCommand { get; set; }
         public ICommand OpenProductInfoLandscapeCommand { get; set; }
         private Models.Product selectedProduct;
         public Models.Product SelectedProduct
@@ -28,15 +32,23 @@ namespace WPFEcommerceApp
                 OnPropertyChanged();
             }
         }
-        public ProductInfoPortraitBannedViewModel(Models.Product product)
+        public ProductInfoPortraitBannedViewModel(AccountStore accountStore, Models.Product product)
         {
+            _accountStore = accountStore;
             SelectedProduct = product;
 
             OpenProductInfoLandscapeCommand = new RelayCommand<object>((p) => { return p != null; }, (p) =>
             {
                 ProductInfoLandscape productInfoLandscape = new ProductInfoLandscape();
-                productInfoLandscape.DataContext = new ProductInfoLandscapeViewModel(SelectedProduct);
-                DialogHost.Show(productInfoLandscape);
+                productInfoLandscape.DataContext = new ProductInfoLandscapeViewModel(_accountStore, SelectedProduct) { IsBanned = true }; ;
+                DialogHost.Show(productInfoLandscape, "App");
+            });
+            ContactUsCommand = new RelayCommandWithNoParameter(() =>
+            {
+                NotificationDialog notificationDialog = new NotificationDialog();
+                notificationDialog.Header = "Contact Info";
+                notificationDialog.ContentDialog = "Please contact us with phone number 0585885214 or email 21520339@uit.gm.edu.vn.";
+                DialogHost.Show(notificationDialog, "App");
             });
         }
     }
