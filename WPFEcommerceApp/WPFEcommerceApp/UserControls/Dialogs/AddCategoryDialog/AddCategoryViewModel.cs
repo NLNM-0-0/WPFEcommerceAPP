7b4fe7e1e07aafdb73e2fae51ef49364fb6e8bf7
NLNM-0-0 <113424382+NLNM-0-0@users.noreply.github.com>
2022-12-20 18:17:12 +0700

    Update: update database to AddCategoryDialog

--- WPFEcommerceApp/WPFEcommerceApp/UserControls/Dialogs/AddCategoryDialog/AddCategoryViewModel.cs
+++ WPFEcommerceApp/WPFEcommerceApp/UserControls/Dialogs/AddCategoryDialog/AddCategoryViewModel.cs
@@ -1,15 +1,20 @@
-﻿using MaterialDesignThemes.Wpf;
+﻿using DataAccessLayer;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using WPFEcommerceApp.Models;
 
 namespace WPFEcommerceApp
 {
     public class AddCategoryDialogViewModel : BaseViewModel
     {
+        private readonly AccountStore _accountStore;
+        private GenericDataRepository<Models.CategoryRequest> categoryRequestReposition =new GenericDataRepository<Models.CategoryRequest>();
         private string categoryName = "";
         public string CategoryName
         {
@@ -30,17 +35,49 @@ namespace WPFEcommerceApp
                 OnPropertyChanged();
             }
         }
+        private string stringCloseDialog = "";
         public ICommand RequestCategoryCommand { get; set; }
-        public AddCategoryDialogViewModel()
+        public ICommand KeyDownEnterCommand { get; set; }
+        public AddCategoryDialogViewModel(AccountStore accountStore)
         {
-            RequestCategoryCommand = new RelayCommand<object>((p) => { return p != null; }, (p) =>
+            _accountStore = accountStore;
+            RequestCategoryCommand = new RelayCommandWithNoParameter(()=>
             {
-                //Lưu dữ liêu xuống data base
-
-                //Tắt dialogHost
-                var closeDialog = DialogHost.CloseDialogCommand;
-                closeDialog.Execute(null, null);
+                Task task = Task.Run(async () => await AddCategoryRequest());
+                while (!task.IsCompleted) ;
+                NotificationDialog notification = new NotificationDialog()
+                {
+                    Header = "Notification",
+                    ContentDialog = stringCloseDialog
+                };
+                DialogHost.Show(notification, "Notification");
             });
+            KeyDownEnterCommand = new RelayCommand<object>((p) => p != null, (p) =>
+            {
+                System.Windows.Controls.Button button = p as System.Windows.Controls.Button;
+                if (button.IsEnabled)
+                {
+                    button.Command.Execute(null);
+                }
+            });
+        }
+        public async Task AddCategoryRequest()
+        {
+            try
+            {
+                await categoryRequestReposition.Add(new CategoryRequest()
+                {
+                    Id = await GenerateID.Gen(typeof(Category)),
+                    IdShop = _accountStore.CurrentAccount.Id,
+                    Name = CategoryName,
+                    Reason = this.Reason
+                }) ;
+                stringCloseDialog = "Update sucessfully. Please wait for us to apply.";
+            }
+            catch
+            {
+                stringCloseDialog = "This category name already exists in Category Request. Please wait for us to accept";
+            }
         }
     }
 }
