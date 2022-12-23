438ab91cca2044cd878f189c4aeb4d8fc5d6a27d
kimthu09 <21521495@gm.uit.edu.vn>
2022-12-23 16:34:11 +0700

    dev: favorite screen

---
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/Favorite/Favorite.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using DataAccessLayer;
+using WPFEcommerceApp.Models;
+
+namespace WPFEcommerceApp
+{
+    /// <summary>
+    /// Interaction logic for Favorite.xaml
+    /// </summary>
+    public partial class Favorite : UserControl
+    {
+        public Favorite()
+        {
+            InitializeComponent();
+            DataContext = new FavoriteViewModel();
+        }
+    }
+}
