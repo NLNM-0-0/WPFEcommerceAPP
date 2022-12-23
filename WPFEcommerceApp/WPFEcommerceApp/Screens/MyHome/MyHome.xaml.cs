10aaa7d7a1ee19b3493638e71ad4649d1bc1cdfd
kimthu09 <21521495@gm.uit.edu.vn>
2022-12-23 16:33:29 +0700

    dev: My home screen

---
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/MyHome/MyHome.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFEcommerceApp
+{
+    /// <summary>
+    /// Interaction logic for MyHome.xaml
+    /// </summary>
+    public partial class MyHome : UserControl
+    {
+        public MyHome()
+        {
+            InitializeComponent();
+            this.DataContext = new MyHomeViewModel();
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var scv = sender as ScrollViewer;
+            if (scv == null) return;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+    }
+}
