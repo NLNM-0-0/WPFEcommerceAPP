f89e93836f6facf5c27f80491a5d8f79537b7c04
vuhoang.vh <21520243@gm.uit.edu.vn>
2022-12-26 03:14:28 +0700

    fix: fill setup

--- WPFEcommerceApp/WPFEcommerceApp/App.xaml.cs
+++ WPFEcommerceApp/WPFEcommerceApp/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using DataAccessLayer;
+using MaterialDesignThemes.Wpf;
 using Microsoft.Extensions.DependencyInjection;
 using WPFEcommerceApp.Models;
 
