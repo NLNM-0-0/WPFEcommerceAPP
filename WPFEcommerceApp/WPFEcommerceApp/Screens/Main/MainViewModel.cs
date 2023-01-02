81f2e534c5da62d4092ac11ef6ab6fa46df3623f
vuhoang.vh <21520243@gm.uit.edu.vn>
2023-01-02 09:17:33 +0700

    update: OnClosing App

--- WPFEcommerceApp/WPFEcommerceApp/Screens/Main/MainViewModel.cs
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/Main/MainViewModel.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using DynamicExpresso;
 using MaterialDesignThemes.Wpf;
 
 namespace WPFEcommerceApp {
@@ -60,7 +61,7 @@ namespace WPFEcommerceApp {
             e.Cancel = true;
             if((sender as Window).WindowState == WindowState.Minimized)
                 (sender as Window).WindowState = WindowState.Normal;
-            var view = new ConfirmDialogCustom() {
+            var view = new ConfirmDialog() {
                 Param = sender,
                 CM = new RelayCommand<object>(t => true, t => {
                     (t as Window).Closing -= MainViewModel.OnClosing;
@@ -69,7 +70,31 @@ namespace WPFEcommerceApp {
                 Header = "Are you sure?",
                 Content = "Your process may not be saved if you close the app. Please check your work before closing the app!"
             };
-            DialogHost.Show(view, "App");
+
+            //need to handle mulltiple identifier
+            UserControl Main = UpdateDialog("Main");
+
+            DialogHost.Show(view, "App", null, (sd, agr) => {
+                if(Main != null)
+                    UpdateDialog("Main", Main);
+            });
+        }
+
+        static UserControl UpdateDialog(string Identifier, UserControl content = null) {
+            DialogSession dialog;
+            try {
+                dialog = DialogHost.GetDialogSession(Identifier);
+            }
+            catch {
+                dialog = null;
+            }
+            if(content != null) {
+                DialogHost.Show(content, Identifier);
+                return null;
+            }
+            content = dialog?.Content as UserControl;
+            if(content != null) dialog.Close();
+            return content;
         }
     }
 }
