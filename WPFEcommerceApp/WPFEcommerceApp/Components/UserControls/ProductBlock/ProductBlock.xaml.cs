fefe080f204a4d833924cdd6c0f24dc1c217c626
NLNM-0-0 <113424382+NLNM-0-0@users.noreply.github.com>
2023-01-04 01:48:31 +0700

    add: heart in product block fix: image margin, size, command when click

--- WPFEcommerceApp/WPFEcommerceApp/Components/UserControls/ProductBlock/ProductBlock.xaml.cs
+++ WPFEcommerceApp/WPFEcommerceApp/Components/UserControls/ProductBlock/ProductBlock.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -26,6 +27,34 @@ namespace WPFEcommerceApp
         public ProductBlock()
         {
             InitializeComponent();
+        }
+        private async void HeartGrid_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(this.DataContext!= null && this.DataContext.GetType() == typeof(ProductBlockViewModel))
+            {
+                ProductBlockViewModel viewModel = (ProductBlockViewModel)this.DataContext;
+                Storyboard favourite = this.Resources["ClickFavouriteProduct"] as Storyboard;
+                Storyboard unFavourite = this.Resources["ClickUnFavouriteProduct"] as Storyboard;
+                viewModel.IsFavorite = !viewModel.IsFavorite;
+                if (viewModel.IsFavorite != null && viewModel.IsFavorite == true)
+                {
+                    if (favourite != null)
+                    {
+                        unFavourite.SkipToFill();
+                        favourite.Begin();
+                    }
+                    await viewModel.UpdateFavoriteProduct();
+                }  
+                else if (viewModel.IsFavorite != null && viewModel.IsFavorite == false)
+                {
+                    if (unFavourite != null)
+                    {
+                        favourite.SkipToFill();
+                        unFavourite.Begin();
+                    }
+                    await viewModel.UpdateFavoriteProduct();
+                }
+            }    
         }
     }
 }
