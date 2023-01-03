073c7249ebecdc89c82176322136e2f3897ab154
NLNM-0-0 <113424382+NLNM-0-0@users.noreply.github.com>
2023-01-04 01:53:37 +0700

    add: Helper contain static int to count when the loading stop

---
+++ WPFEcommerceApp/WPFEcommerceApp/Utils/IsLoadingCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFEcommerceApp
+{
+    public static class IsLoadingCheck
+    {
+        private static int isLoading;
+        public static int IsLoading
+        {
+            get { return isLoading; }
+            set
+            {
+                isLoading = value;
+                if(isLoading > 0)
+                {
+                    MainViewModel.IsLoading = true;
+                }
+                else
+                {
+                    MainViewModel.IsLoading = false;
+                }
+            }
+        }
+    }
+}
