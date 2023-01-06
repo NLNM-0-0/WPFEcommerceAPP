9f016235c535e55bafe97ff90535c257e032d6f7
vuhoang.vh <21520243@gm.uit.edu.vn>
2023-01-06 08:26:32 +0700

    dev: OAuth api, login with google

---
+++ WPFEcommerceApp/WPFEcommerceApp/Api/OAuth.cs
@@ -0,0 +1,273 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace WPFEcommerceApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public class OAuth {
+        public async Task<Tuple<string, object>> Authentication() {
+            // Generates state and PKCE values.
+            string state = randomDataBase64url(32);
+            string code_verifier = randomDataBase64url(32);
+            string code_challenge = base64urlencodeNoPadding(sha256(code_verifier));
+            const string code_challenge_method = "S256";
+
+            // Creates a redirect URI using an available port on the loopback address.
+            string redirectURI = string.Format("http://{0}:{1}/", IPAddress.Loopback, GetRandomUnusedPort());
+            //output("redirect URI: " + redirectURI);
+
+            // Creates an HttpListener to listen for requests on that redirect URI.
+            var http = new HttpListener();
+            http.Prefixes.Add(redirectURI);
+            //output("Listening..");
+            http.Start();
+
+            // Creates the OAuth 2.0 authorization request.
+            // Scope is declare in here
+            var scope = CreateScopes(scopes);
+            string authorizationRequest = string.Format("{0}?response_type=code&scope={1}&redirect_uri={2}&client_id={3}&state={4}&code_challenge={5}&code_challenge_method={6}",
+                authorizationEndpoint,
+                scope,
+                System.Uri.EscapeDataString(redirectURI),
+                clientID,
+                state,
+                code_challenge,
+                code_challenge_method);
+
+            // Opens request in the browser.
+            System.Diagnostics.Process.Start(authorizationRequest);
+
+            // Waits for the OAuth authorization response.
+            var context = await http.GetContextAsync();
+
+            // Sends an HTTP response to the browser.
+            var response = context.Response;
+            string responseString = string.Format(
+                "<html><head><meta http-equiv='refresh' content='10;url=https://google.com'></head><body>Return to the WANO....</body></html>"
+                );
+            var buffer = Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            var responseOutput = response.OutputStream;
+            Task responseTask = responseOutput.WriteAsync(buffer, 0, buffer.Length).ContinueWith((task) => {
+                responseOutput.Close();
+                http.Stop();
+                Console.WriteLine("HTTP server stopped.");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            // Checks for errors.
+            if(context.Request.QueryString.Get("error") != null) {
+                output(String.Format("OAuth authorization error: {0}.", context.Request.QueryString.Get("error")));
+                return new Tuple<string, object>(null, null);
+            }
+            if(context.Request.QueryString.Get("code") == null
+                || context.Request.QueryString.Get("state") == null) {
+                output("Malformed authorization response. " + context.Request.QueryString);
+                return new Tuple<string, object>(null, null);
+            }
+
+            // extracts the code
+            var code = context.Request.QueryString.Get("code");
+            var incoming_state = context.Request.QueryString.Get("state");
+
+            // Compares the receieved state to the expected value, to ensure that
+            // this app made the request which resulted in authorization.
+            if(incoming_state != state) {
+                output(String.Format("Received request with invalid state ({0})", incoming_state));
+                return new Tuple<string, object>(null, null);
+            }
+            //output("Authorization code: " + code);
+
+            // Starts the code exchange at the Token Endpoint.
+            return await performCodeExchange(code, code_verifier, redirectURI);
+        }
+
+        async Task<Tuple<string, object>> performCodeExchange(string code, string code_verifier, string redirectURI) {
+            //output("Exchanging code for tokens...");
+
+            // builds the  request
+            string tokenRequestURI = tokenEndpoint;
+            string tokenRequestBody = string.Format("code={0}&redirect_uri={1}&client_id={2}&code_verifier={3}&client_secret={4}&scope=&grant_type=authorization_code",
+                code,
+                Uri.EscapeDataString(redirectURI),
+                clientID,
+                code_verifier,
+                clientSecret
+                );
+
+            // sends the request
+            HttpWebRequest tokenRequest = (HttpWebRequest)WebRequest.Create(tokenRequestURI);
+            tokenRequest.Method = "POST";
+            tokenRequest.ContentType = "application/x-www-form-urlencoded";
+            tokenRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            byte[] _byteVersion = Encoding.ASCII.GetBytes(tokenRequestBody);
+            tokenRequest.ContentLength = _byteVersion.Length;
+            Stream stream = tokenRequest.GetRequestStream();
+            await stream.WriteAsync(_byteVersion, 0, _byteVersion.Length);
+            stream.Close();
+
+            Tuple<string, object> res = new Tuple<string, object>(null, null);
+            object Item3 = null;
+            try {
+                // gets the response
+                WebResponse tokenResponse = await tokenRequest.GetResponseAsync();
+                using(StreamReader reader = new StreamReader(tokenResponse.GetResponseStream())) {
+                    // reads response body
+                    string responseText = await reader.ReadToEndAsync();
+                    //output(responseText);
+
+                    // converts to dictionary
+                    Dictionary<string, string> tokenEndpointDecoded = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseText);
+
+                    string access_token = tokenEndpointDecoded["access_token"];
+                    string refresh_token = tokenEndpointDecoded["refresh_token"];
+                    res = new Tuple<string, object>(refresh_token, null);
+                    Item3 = await userinfoCall(access_token);
+                }
+            } catch(WebException ex) {
+                if(ex.Status == WebExceptionStatus.ProtocolError) {
+                    var response = ex.Response as HttpWebResponse;
+                    if(response != null) {
+                        output("HTTP: " + response.StatusCode);
+                        using(StreamReader reader = new StreamReader(response.GetResponseStream())) {
+                            // reads response body
+                            string responseText = await reader.ReadToEndAsync();
+                            output(responseText);
+                        }
+                        return new Tuple<string, object>(null, null);
+                    }
+
+                }
+            }
+            return new Tuple<string, object>(res.Item1, Item3);
+        }
+
+        async Task<object> userinfoCall(string access_token) {
+            //output("Making API Call to Userinfo...");
+
+            // builds the  request
+            string userinfoRequestURI = userInfoEndpoint;
+
+            try {
+                HttpWebRequest userinfoRequest = (HttpWebRequest)WebRequest.Create(userinfoRequestURI);
+                userinfoRequest.Method = "GET";
+                userinfoRequest.Headers.Add(string.Format("Authorization: Bearer {0}", access_token));
+                userinfoRequest.ContentType = "application/x-www-form-urlencoded";
+                userinfoRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+                // gets the response
+                WebResponse userinfoResponse = await userinfoRequest.GetResponseAsync();
+                using(StreamReader userinfoResponseReader = new StreamReader(userinfoResponse.GetResponseStream())) {
+                    // reads response body
+                    string userinfoResponseText = await userinfoResponseReader.ReadToEndAsync();
+                    return await getUserDetail(userinfoResponseText, access_token);
+                }
+            } catch { Debug.WriteLine("UserInfo"); return null; }
+        }
+
+        async Task<object> getUserDetail(string json, string access_token) {
+            var jsonParse = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            string userinfoRequestURI = CreatePeopleEndpoint(jsonParse["sub"], personFields);
+
+            // sends the request
+            try {
+                HttpWebRequest userinfoRequest = (HttpWebRequest)WebRequest.Create(userinfoRequestURI);
+                userinfoRequest.Method = "GET";
+                userinfoRequest.Headers.Add(string.Format("Authorization: Bearer {0}", access_token));
+                userinfoRequest.ContentType = "application/x-www-form-urlencoded";
+                userinfoRequest.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+                // gets the response
+                WebResponse userinfoResponse = await userinfoRequest.GetResponseAsync();
+                using(StreamReader userinfoResponseReader = new StreamReader(userinfoResponse.GetResponseStream())) {
+                    // reads response body
+                    string userinfoResponseText = await userinfoResponseReader.ReadToEndAsync();
+                    var parse = JsonConvert.DeserializeObject<Dictionary<string, object>>(userinfoResponseText);
+                    return new Tuple<object, object>(jsonParse, parse);
+                }
+            } catch { Debug.WriteLine("UserDetail"); return null; }
+        }
+
+        #region Methods
+        public static int GetRandomUnusedPort() {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+        void output(string output) {
+            Console.WriteLine(output);
+        }
+
+        string randomDataBase64url(uint length) {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] bytes = new byte[length];
+            rng.GetBytes(bytes);
+            return base64urlencodeNoPadding(bytes);
+        }
+
+        byte[] sha256(string inputStirng) {
+            byte[] bytes = Encoding.ASCII.GetBytes(inputStirng);
+            SHA256Managed sha256 = new SHA256Managed();
+            return sha256.ComputeHash(bytes);
+        }
+
+        string base64urlencodeNoPadding(byte[] buffer) {
+            string base64 = Convert.ToBase64String(buffer);
+
+            // Converts base64 to base64url.
+            base64 = base64.Replace("+", "-");
+            base64 = base64.Replace("/", "_");
+            // Strips padding.
+            base64 = base64.Replace("=", "");
+
+            return base64;
+        }
+        #endregion
+        #region client configuration
+        const string clientID = "129105441268-ljk3ijg5f7nulvac5qi9dm0hiqq8rlqf.apps.googleusercontent.com";
+        const string clientSecret = "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";
+        const string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+        const string tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
+        const string userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+        const string peopleEndpoint = "https://people.googleapis.com/v1/people/";
+        string CreatePeopleEndpoint(string id, string[] personFields) {
+            string res = peopleEndpoint;
+            res += id + "?personFields=";
+            for(int i = 0; i < personFields.Length - 1; i++) {
+                res += personFields[i] + ",";
+            }
+            res += personFields[personFields.Length-1];
+            return res;
+        }
+        string CreateScopes(string[] scopes) {
+            string x = "";
+            for(int i = 0; i < scopes.Length - 1; i++) {
+                x += scopes[i] + "%20";
+            }
+            x += scopes[scopes.Length-1];
+            return x;
+        }
+        string[] personFields = { "genders", "birthdays", "coverPhotos", "phoneNumbers" };
+        string[] scopes = {
+                "https://www.googleapis.com/auth/user.birthday.read",
+                "https://www.googleapis.com/auth/user.gender.read",
+                "https://www.googleapis.com/auth/user.phonenumbers.read",
+                "https://www.googleapis.com/auth/userinfo.email",
+                "https://www.googleapis.com/auth/userinfo.profile"
+        };
+        #endregion
+    }
+}
--- WPFEcommerceApp/WPFEcommerceApp/Screens/Auth/Login/LoginViewModel.cs
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/Auth/Login/LoginViewModel.cs
@@ -1,18 +1,23 @@
 ﻿using DataAccessLayer;
 using MaterialDesignThemes.Wpf;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Xml.Linq;
 using WPFEcommerceApp.Models;
 
 namespace WPFEcommerceApp {
-    internal class LoginViewModel : BaseViewModel {
-        public GenericDataRepository<Models.UserLogin> UserRepository { get; set; }
+    public class LoginViewModel : BaseViewModel {
+        private readonly GenericDataRepository<Models.MUser> userRepo = new GenericDataRepository<MUser>();
+        private readonly GenericDataRepository<Models.UserLogin> loginRepo = new GenericDataRepository<UserLogin>();
+
 
         private ObservableCollection<Models.UserLogin> accounts;
         public ObservableCollection<Models.UserLogin> Accounts {
@@ -35,8 +40,8 @@ namespace WPFEcommerceApp {
         public ICommand OnLogin { get; set; }
         public ICommand CloseCM { get; set; }
         public ICommand OnSignUp { get; set; }
+        public ICommand OnGoogleSignIn { get; set; }
         public LoginViewModel(object o) {
-            UserRepository = new GenericDataRepository<Models.UserLogin>();
 
             //_accountStore.CurrentAccount = ;
             OnLogin = new RelayCommand<object>(p => true, async p => {
@@ -59,11 +64,119 @@ namespace WPFEcommerceApp {
                 };
                 DialogHost.Show(register, "Login");
             });
+            OnGoogleSignIn = new ImmediateCommand<object>(async p => {
+                var auth = new OAuth();
+                (p as Window).Activate();
+                var x = await auth.Authentication();
+                Debug.WriteLine(x);
+                if(x.Item1 == null || x.Item2 == null) {
+                    ErrorMessage();
+                    return;
+                }
+                try {
+                    var ins1 = (x.Item2 as Tuple<object, object>).Item1 as Dictionary<string, string>;
+                    var ins2 = (x.Item2 as Tuple<object, object>).Item2 as Dictionary<string, object>;
+                    string birthdays = null, coverPhotos = null, phoneNumbers = null, genders = null;
+                    try {
+                        birthdays = ins2["birthdays"]?.ToString();
+                        coverPhotos = ins2["coverPhotos"]?.ToString();
+                        phoneNumbers = ins2["phoneNumbers"]?.ToString();
+                        genders = ins2["genders"]?.ToString();
+                    } catch { }
+                    if(birthdays != null) {
+                        Stdize(ref birthdays);
+                        var bd = JsonConvert.DeserializeObject<Dictionary<string, object>>(birthdays);
+                        var bd2 = JsonConvert.DeserializeObject<Dictionary<string, string>>((bd as Dictionary<string, object>)["date"].ToString());
+                        ins1["birthday"] = bd2["month"] + '/' + bd2["day"] + '/' + bd2["year"];
+                    }
+                    if(coverPhotos != null) {
+                        Stdize(ref coverPhotos);
+                        var cp = JsonConvert.DeserializeObject<Dictionary<string, object>>(coverPhotos);
+                        ins1["coverPhoto"] =  (cp as Dictionary<string, object>)["url"].ToString();
+                    }
+                    if(phoneNumbers != null) {
+                        //handle here
+                    }
+                    if(genders != null) {
+                        //handle here
+                    }
+                    var check = await loginRepo.GetSingleAsync(d => d.IdUser == ins1["sub"]);
+                    if(check == null) {
+                        await CreateAccount(ins1);
+                    }
+                    var user = await userRepo.GetSingleAsync(d => d.Id == ins1["sub"], d => d.Products1);
+                    AccountStore.instance.CurrentAccount = user;
+                    (p as Window).Hide();
+                    (o as Window).WindowState = WindowState.Minimized;
+                    (o as Window).Show();
+                    (o as Window).WindowState = WindowState.Normal;
+                    (p as Window).Close();
+
+                } catch { ErrorMessage(); }
+            });
+        }
+
+        public async Task<bool> CreateAccount(Dictionary<string, string> user) {
+            try {
+                string phoneNumber = "", gender = null, cover = null, birthday = null;
+                user.TryGetValue("phoneNumber",out phoneNumber);
+                user.TryGetValue("gender", out gender);
+                user.TryGetValue("coverPhoto", out cover);
+                user.TryGetValue("birthday", out birthday);
+                if(phoneNumber == null) {
+                    phoneNumber = "";
+                }
+                bool flag = true;
+                DateTime t = DateTime.Now;
+                try {
+                    Convert.ToDateTime(birthday);
+                } catch { flag = false; }
+                var us = new Models.MUser() {
+                    Id = user["sub"],
+                    Name = user["name"],
+                    Email = user["email"],
+                    PhoneNumber = phoneNumber,
+                    Gender = gender == "Nam" ? true : false,
+                    SourceImageAva = user["picture"],
+                    SourceImageBackground = cover,
+                    Address = "",
+                    Description = "",
+                    StatusShop = "NotExist",
+                    StatusUser = "NotBanned",
+                    Role = "User"
+                };
+                if(flag) us.DOB = Convert.ToDateTime(birthday);
+
+                await userRepo.Add(us);
+                await loginRepo.Add(new Models.UserLogin() {
+                    IdUser = user["sub"],
+                    Password = null,
+                    Username = user["email"]
+                });
+            } catch(Exception e) {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+            return true;
+        }
+
+        void Stdize(ref string t) {
+            if(t[0] == '[') {
+                t = t.Remove(0, 1);
+                t = t.Remove(t.Length - 1, 1);
+            }
+        }
+        void ErrorMessage() {
+            var dlg = new ConfirmDialog() {
+                Header = "Whoops",
+                Content = "Something is wrong, please try again!"
+            };
+            DialogHost.Show(dlg, "Login");
         }
         private async Task<bool> Login() {
             var encode = new Hashing().Encrypt(Username, Password);
 
-            UserLogin acc = await UserRepository.GetSingleAsync(
+            UserLogin acc = await loginRepo.GetSingleAsync(
                 x => (x.Username == username
                 && x.Password == encode),
                 x => x.MUser);
@@ -85,3 +198,4 @@ namespace WPFEcommerceApp {
         }
     }
 }
+
