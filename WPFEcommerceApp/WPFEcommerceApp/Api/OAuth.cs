8a8086bc919985626c596c63b5b1b67115191066
vuhoang.vh <21520243@gm.uit.edu.vn>
2023-01-08 05:26:09 +0700

    dev: RadioButtonCard, GenId by Datetime

--- WPFEcommerceApp/WPFEcommerceApp/Api/OAuth.cs
+++ WPFEcommerceApp/WPFEcommerceApp/Api/OAuth.cs
@@ -66,7 +66,7 @@ namespace WPFEcommerceApp {
             // Sends an HTTP response to the browser.
             var response = context.Response;
             string responseString = string.Format(
-                "<html><head><meta http-equiv='refresh' content='1;url=https://google.com'></head><body><h1>Return to the WANO....</h1></body></html>"
+                "<html><head><meta http-equiv='refresh' content='2;url=https://google.com'></head><body><h2>Return to the WANO....</h2></body></html>"
                 );
             var buffer = Encoding.UTF8.GetBytes(responseString);
             response.ContentLength64 = buffer.Length;
