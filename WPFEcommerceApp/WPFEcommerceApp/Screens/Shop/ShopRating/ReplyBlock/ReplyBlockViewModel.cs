27335331a97d7f7b9cbea95da0b63572e9a44bd9
Mai <113424382+NLNM-0-0@users.noreply.github.com>
2023-01-08 19:01:12 +0700

    update: Ui add: feature comment

---
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/Shop/ShopRating/ReplyBlock/ReplyBlockViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFEcommerceApp
+{
+    public class ReplyBlockViewModel: BaseViewModel
+    {
+        private Models.RatingInfo ratingInfo;
+        public Models.RatingInfo RatingInfo
+        {
+            get { return ratingInfo; }
+            set
+            {
+                ratingInfo = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(SourceImageAva));
+            }
+        }
+
+        public string SourceImageAva
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(ratingInfo.MUser.SourceImageAva))
+                {
+                    return Properties.Resources.DefaultShopAvaImage;
+                }
+                return ratingInfo.MUser.SourceImageAva;
+            }
+        }
+        public ReplyBlockViewModel(Models.RatingInfo ratingInfo)
+        {
+            RatingInfo = ratingInfo;
+        }
+    }
+}
