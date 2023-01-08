8a898d5590b1552fc0a2990df96d23dddcc82d8e
kimthu09 <21521495@gm.uit.edu.vn>
2023-01-08 23:01:03 +0700

    update: address view in profile

--- WPFEcommerceApp/WPFEcommerceApp/Screens/General/MyProfile/MyProfileViewModel.cs
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/General/MyProfile/MyProfileViewModel.cs
@@ -2,6 +2,7 @@
 using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Security.AccessControl;
@@ -23,7 +24,15 @@ namespace WPFEcommerceApp
         #region Public Properties
 
         private GenericDataRepository<MUser> userRepo;
+        private GenericDataRepository<Address> addressRepo;
         public MUser EditUser { get; set; }
+        public bool IsAdmin
+        {
+            get => AccountStore.instance.CurrentAccount != null
+                && AccountStore.instance.CurrentAccount.Role == "Admin";
+        }
+
+        public ObservableCollection<AddressItemViewModel> AddressList { get; set; }
 
         public string SourceImageBackground
         {
@@ -88,14 +97,13 @@ namespace WPFEcommerceApp
         public ICommand ChangeBackgroundShopCommand { get; set; }
         public ICommand ChangeToDefaultBackgroundShopCommand { get; set; }
         public ICommand SaveBackgroundShopCommand { get; set; }
-
         public ICommand SaveProfileCommand { get; set; }
         public ICommand SaveAvaShopCommand { get; set; }
-
         public ICommand CancelProfileCommand { get; set; }
-
         public ICommand OpenAvaDialog { get; set; }
         public ICommand OpenBackgroundDialog { get; set; }
+        public ICommand AddAddressCommand { get; set; }
+
 
         #endregion
 
@@ -104,7 +112,7 @@ namespace WPFEcommerceApp
         public MyProfileViewModel()
         {
             userRepo = new GenericDataRepository<MUser>();
-
+            addressRepo = new GenericDataRepository<Address>();
 
             OpenAvaDialog = new RelayCommandWithNoParameter(async () =>
             {
@@ -127,20 +135,55 @@ namespace WPFEcommerceApp
             {
                 MainViewModel.IsLoading = true;
                 await Load();
+            }).ContinueWith((task) =>
+            {
                 MainViewModel.IsLoading = false;
             });
 
             SaveProfileCommand = new RelayCommand<object>(p => p != null, async (p) => await SaveProfile(p));
             CancelProfileCommand = new RelayCommand<object>(p => p != null, async (p) => await CancelProfile(p));
+            AddAddressCommand = new RelayCommandWithNoParameter(async () => await OpenAddAddressDialog());
+            AddressDialogViewModel.AddressCommand = new RelayCommand<object>(p => p != null, async (p) => await AddNewAddress(p));
+            AddressItemViewModel.EditAddressCommand = new RelayCommand<object>(p => p != null, async (p) => await OpenEditAddress(p));
+            AddressDialogViewModel.RemoveAddressCommand = new RelayCommand<object>(p => p != null, async (p) => await RemoveAddress(p));
         }
         #endregion
 
         #region Command Methods
         public async Task Load()
         {
-            EditUser = await userRepo.GetSingleAsync(usr => usr.Id == AccountStore.instance.CurrentAccount.Id);
+            EditUser = await userRepo.GetSingleAsync(usr => usr.Id == AccountStore.instance.CurrentAccount.Id,
+                usr => usr.Addresses
+                );
 
+            if (AccountStore.instance != null)
+            {
+                AccountStore.instance.AccountChanged += OnAccountChange;
+            }
 
+            App.Current.Dispatcher.Invoke((Action)(() =>
+            {
+                AddressList = new ObservableCollection<AddressItemViewModel>();
+                foreach (var item in EditUser.Addresses)
+                {
+                    if (item.Status != true)
+                        continue;
+
+                    bool isDefault = false;
+                    if (item.Id == EditUser.DefaultAddress)
+                    {
+                        isDefault = true;
+                        AddressList.Insert(0, new AddressItemViewModel(item, isDefault));
+                        continue;
+                    }
+                    AddressList.Add(new AddressItemViewModel(item, isDefault));
+                }
+            }));
+        }
+
+        private void OnAccountChange()
+        {
+            OnPropertyChanged(nameof(IsAdmin));
         }
 
         private async void SaveAva(object sender, DialogClosedEventArgs eventArgs)
@@ -161,6 +204,7 @@ namespace WPFEcommerceApp
                 }
 
                 AccountStore.instance.CurrentAccount.SourceImageAva = link;
+                OnPropertyChanged(nameof(SourceImageAva));
                 await AccountStore.instance.Update(AccountStore.instance.CurrentAccount);
                 await Load();
             }
@@ -186,6 +230,7 @@ namespace WPFEcommerceApp
                 }
 
                 AccountStore.instance.CurrentAccount.SourceImageBackground = link;
+                OnPropertyChanged(nameof(SourceImageBackground));
                 await AccountStore.instance.Update(AccountStore.instance.CurrentAccount);
                 await Load();
             }
@@ -217,7 +262,126 @@ namespace WPFEcommerceApp
             MainViewModel.IsLoading = false;
 
         }
+
+        public async Task OpenAddAddressDialog()
+        {
+            var dialog = new AddAddressDialog();
+            var add = new Address();
+            dialog.DataContext = new AddressDialogViewModel
+            {
+                Header = "Add new address",
+                CommandContent = "Add",
+                Address = add,
+                IsAdding = true,
+                IsDefault = false,
+            };
+
+            await DialogHost.Show(dialog, "Main", null, null, null);
+        }
+
+        public async Task OpenEditAddress(object obj)
+        {
+            var dialog = new AddAddressDialog();
+            var add = obj as AddressItemViewModel;
+
+            if (add == null)
+                return;
+
+            dialog.DataContext = new AddressDialogViewModel
+            {
+                Header = "Edit address",
+                CommandContent = "Save",
+                Address = add.Address,
+                IsAdding = false,
+                IsDefault = add.IsDefault,
+            };
+
+            await DialogHost.Show(dialog, "Main", null, null, Reload);
+        }
+
+        private async void Reload(object sender, DialogClosedEventArgs eventArgs)
+        {
+            await Load();
+        }
+
+        public async Task AddNewAddress(object obj)
+        {
+            DialogHost.CloseDialogCommand.Execute(null, null);
+
+            MainViewModel.IsLoading = true;
+            var ad = obj as AddressDialogViewModel;
+            if (ad == null)
+                return;
+
+            var address = ad.Address;
+
+            if (ad.IsSetAsDefault)
+            {
+                AccountStore.instance.CurrentAccount.DefaultAddress = ad.Address.Id;
+                await AccountStore.instance.Update(AccountStore.instance.CurrentAccount);
+            }
+
+            if (ad.IsAdding)
+            {
+                address.IdUser = AccountStore.instance.CurrentAccount.Id;
+                address.Status = true;
+                address.Id = GetAddressId();
+                await addressRepo.Add(address);
+            }
+            else
+            {
+                await addressRepo.Update(address);
+            }
+
+            await Load();
+            MainViewModel.IsLoading = false;
+
+        }
+        public async Task RemoveAddress(object obj)
+        {
+            DialogHost.CloseDialogCommand.Execute(null, null);
+            MainViewModel.IsLoading = true;
+
+            var add = obj as AddressDialogViewModel;
+            if (add == null)
+                return;
+
+            if (add.IsDefault)
+            {
+                MainViewModel.IsLoading = false;
+
+                var view = new ConfirmDialog
+                {
+                    Header = "No!",
+                    Content = "You cannot remove default address"
+                };
+                await DialogHost.Show(view, "Main");
+            }
+            else
+            {
+                add.Address.Status = false;
+                await addressRepo.Update(add.Address);
+                await Load();
+            }
+            MainViewModel.IsLoading = false;
+
+        }
+
+
         #endregion
+
+        public override void Dispose()
+        {
+            AccountStore.instance.AccountChanged -= OnAccountChange;
+            base.Dispose();
+        }
+
+        public static string GetAddressId()
+        {
+            var now = DateTime.Now;
+            var temp = now.Year + now.Month + now.Day + now.Hour + now.Minute + now.Second;
+            return temp.ToString();
+        }
     }
 
 
