d30a87f4825e58c25d0150165192bc07a0af87f5
vuhoang.vh <21520243@gm.uit.edu.vn>
2023-01-09 23:35:15 +0700

    fix: PromoInformation constructor (VHCMT for more info)

--- WPFEcommerceApp/WPFEcommerceApp/Screens/Shop/ShopPromo/PromoInfomation/PromoInformation.xaml.cs
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/Shop/ShopPromo/PromoInfomation/PromoInformation.xaml.cs
@@ -21,22 +21,9 @@ namespace WPFEcommerceApp
     /// </summary>
     public partial class PromoInformation : UserControl
     {
-        Models.Promo promo = new Models.Promo();
         public PromoInformation()
         {
-            Task.Run(async () => await Load()).Wait();
-            this.DataContext = new PromoInformationViewModel(promo);
             InitializeComponent();
-        }
-        public async Task Load()
-        {
-            GenericDataRepository<Models.Promo> genericDataRepository= new GenericDataRepository<Models.Promo>();
-            promo = await genericDataRepository.GetSingleAsync(p=>p.Id == "8##QZd6w", 
-                                                                p => p.Products,
-                                                                p => p.MUser,
-                                                                p => p.Products.Select(temp => temp.ImageProducts),
-                                                                p => p.Products.Select(temp => temp.Brand),
-                                                                p => p.Products.Select(temp => temp.Category));
         }
     }
 }
