57b4de21bd692f01d95cbbd4949d339991a92220
vuhoang.vh <21520243@gm.uit.edu.vn>
2023-01-17 10:37:30 +0700

    fix: UI (orderCard, Review), add discount in DB

---
+++ WPFEcommerceApp/WPFEcommerceApp/Screens/General/Order/components/DescriptionConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFEcommerceApp {
+    internal class DescriptionConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            string description = (string)value;
+            description = description.Trim();
+            description = description.Substring(0, 196);
+            description += "...";
+            return description;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
